f628c25a8f1b2c45004cbf50b6e2958506af06e4
Sitharamshiva <164468134+Sitharamshiva@users.noreply.github.com>
2024-07-10 17:05:51 +0530

    main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-st.image(r"C:\Users\shiva\Downloads\inno image.jpeg")
+st.image(r"inno image.jpeg")
 
-model=pickle.load(open(r"C:\Users\shiva\MACHINE LEARNING\nb.pkl","rb"))
-model1=pickle.load(open(r"C:\Users\shiva\MACHINE LEARNING\nb1.pkl","rb"))
+model=pickle.load(open(r"nb.pkl","rb"))
+model1=pickle.load(open(r"nb1.pkl","rb"))
 
 
 
@@ -24,10 +24,10 @@ prediciton=model.predict(checking)[0]
 if st.button('Classify'):
     if prediciton=="spam":
         st.write("the email is spam")
-        st.image(r"C:\Users\shiva\spam image.jpeg")
+        st.image(r"spam image.jpeg")
       
     else:
-        st.image(r"C:\Users\shiva\ham.png")
+        st.image(r"ham.png")
 
 else:
      st.write("this email is","ham")
